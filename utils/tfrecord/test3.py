0f28499ba60e71d1f02a28691b93f4e444aa6f66
gavinlzhang <13475441+gavinlzhang@user.noreply.gitee.com>
2026-04-13 23:21:00 -0400

    first commit

--- utils/tfrecord/test3.py
+++
@@ -1,7 +0,0 @@
-﻿import numpy as np
-# import tensorflow as tf
-# from deepctr.models import DeepFM
-# from deepctr.feature_column import SparseFeat, get_feature_names
-
-pretrained_item_weights = np.random.randn(60, 4)
-print(pretrained_item_weights)
--- utils/tfrecord/xxx.py
+++
@@ -1,6 +0,0 @@
-﻿#!/usr/bin/python3
-# coding=utf-8
-list1 = {'Google': 1, 'Runoob': 2, 'Taobao': 3}
-list_pop = list1.pop('Runoob')
-print("鍒犻櫎鐨勯」涓?:", list_pop)
-print("鍒楄〃鐜板湪涓?: ", list1)
